8258ee8113a646fb4884f8d008132b8dce7e31b6
Thales Lopes <thalesbrl@outlook.com>
2025-09-23 23:50:59 +0100

    update(create-header-component): finishing header functionalities Changes made:     - Finishing header design     - Allow light/dark mode button change background

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import Header from "./components/header/Header.tsx";
 import { useState } from "react";
 
 function App() {
-  const [insLightMode, setInsLightMode] = useState(true);
-  const toggleMode = () => setInsLightMode(!insLightMode);
+  const [mode, setMode] = useState<"light" | "dark" | "animating">("light");
+
+  const toggleMode = () => {
+    setMode((prev) => (prev === "light" ? "dark" : "light"));
+  };
 
   return (
-    <div className="w-screen h-screen bg-[#0E0D0D]">
-      <Header isLightMode={insLightMode} toggleMode={toggleMode} />
+    <div
+      className={`
+        w-screen h-screen
+        ${mode === "light" ? "bg-[#FF8888]" : ""}
+        ${mode === "dark" ? "bg-[#06001D]" : ""}
+      `}
+    >
+      <Header mode={mode} toggleMode={toggleMode} />
     </div>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,13 +3,13 @@ import { RxDownload } from "react-icons/rx";
 import MyLogo from "./../../../public/my-logo.png";
 
 type HeaderProps = {
-  isLightMode: boolean;
+  mode: "light" | "dark" | "animating";
   toggleMode: () => void;
 };
 
-function Header({ isLightMode, toggleMode }: HeaderProps) {
+function Header({ mode, toggleMode }: HeaderProps) {
   return (
-    <div className="flex flex-row w-full h-[13%]">
+    <div className="flex flex-row w-full h-[14%] fixed">
       <div className="w-1/2 flex items-center pl-[5%]">
         <img
           src={MyLogo}
@@ -17,10 +17,18 @@ function Header({ isLightMode, toggleMode }: HeaderProps) {
           className="w-1/12 hover:w-1/11 transition-all duration-300"
         />
       </div>
-      <div className="w-1/2 flex justify-end items-center gap-[10%] pr-[5%]">
+      <div className="w-1/2 flex justify-end items-center gap-[8%] pr-[5%]">
         <div>
-          <button className="relative group flex items-center bg-indigo-200 px-4 py-3 rounded-xl hover:scale-103 transition-all active:scale-98 overflow-hidden hover:text-orange-500">
-            <span className=" absolute top-[200%] left-0 w-full h-full bg-white rounded-[50%] group-hover:top-0 transition-all duration-700 scale-150 z-0"></span>
+          <button
+            className="
+              relative group flex items-center px-4 py-3 rounded-xl transition-all duration-400 overflow-hidden
+              bg-indigo-400
+              text-white
+              hover:scale-103 hover:text-black/50
+              active:scale-95
+              "
+          >
+            <span className="absolute top-[200%] left-0 w-full h-full bg-white rounded-[50%] group-hover:top-0 transition-all ease-in-out duration-800 scale-150 z-0"></span>
 
             <div className="flex items-center gap-2 z-10">
               <RxDownload size={30} />
@@ -30,10 +38,12 @@ function Header({ isLightMode, toggleMode }: HeaderProps) {
         </div>
         <div>
           <button
-            className="p-[35%] bg-indigo-200 rounded-full transition-all duration-300 hover:scale-108  hover:shadow-white hover:shadow hover:text-orange-500 active:scale-95"
+            className="p-[35%] bg-white rounded-full transition-all duration-200 hover:scale-110 hover:text-white hover:bg-indigo-400 active:scale-100"
             onClick={toggleMode}
+            disabled={mode === "animating"}
           >
-            {isLightMode ? <PiMoon size={24} /> : <PiSunLight size={24} />}
+            {mode === "light" && <PiSunLight size={24} />}
+            {mode === "dark" && <PiMoon size={24} />}
           </button>
         </div>
       </div>
