4353ff3f1c48c8852f870812a3b24f49749cae29
Thales Lopes <thalesbrl@outlook.com>
2025-09-27 13:55:54 +0100

    feat(background-animation): finishing styling text design

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,17 +5,17 @@ type BackgroundProps = {
 
 function Hero({ mode }: BackgroundProps) {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-full gap-10 pt-[10%] z-10">
-      <div className="flex flex-row text-4xl gap-3 font-space">
+    <div className="flex flex-col items-center justify-center w-full h-full gap-5 pt-[12%] z-10">
+      <div className="flex flex-row gap-6 font-space items-baseline">
         <div
-          className={`duration-3000 ease-linear text-shadow-lg text-balance
+          className={`duration-3000 ease-linear text-shadow-lg text-balance text-[2rem]
                     ${mode === "light" ? "text-black text-shadow-black/20" : "text-white text-shadow-white/40"}
                 `}
         >
           Hi. I'm
         </div>
         <div
-          className={`duration-3000 ease-linear text-shadow-lg animate-scalePulse text-balance
+          className={`duration-3000 ease-linear text-shadow-lg animate-scalePulse text-balance text-[2.5rem]
                     ${mode === "light" ? "text-[#571111] text-shadow-black/20" : "text-[#60A5FA] text-shadow-white/40"}
                 `}
         >
@@ -25,7 +25,7 @@ function Hero({ mode }: BackgroundProps) {
       <div
         className={`font-space duration-3000 ease-linear text-balance
             text-shadow-lg text-[3rem]
-            ${mode === "light" ? "text-[#A87777] text-shadow-black/40" : "text-[#60A5FA] text-shadow-white/40"} 
+            ${mode === "light" ? "text-[#A87777] text-shadow-black/40" : "text-[#95C1F8] text-shadow-white/60"} 
             `}
       >
         <TypeAnimation
