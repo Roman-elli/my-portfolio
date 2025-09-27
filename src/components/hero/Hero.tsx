a73c4b1dc6997e35e47f44da1423df81bbcc6156
Thales Lopes <thalesbrl@outlook.com>
2025-09-27 13:07:20 +0100

    chore(feat/background-animation): added more files in gitignore

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,52 @@
+import { TypeAnimation } from "react-type-animation";
+type BackgroundProps = {
+  mode: "light" | "dark";
+};
+
+function Background({ mode }: BackgroundProps) {
+  return (
+    <div
+      className={`flex flex-col items-center justify-center w-full h-full gap-10 pt-[10%] 
+      `}
+    >
+      <div className="flex flex-row text-4xl gap-3 font-space">
+        <div
+          className={`duration-3000 ease-linear text-shadow-lg text-balance
+                    ${mode === "light" ? "text-black text-shadow-black/20" : "text-white text-shadow-white/40"}
+                `}
+        >
+          Hi. I'm
+        </div>
+        <div
+          className={`duration-3000 ease-linear text-shadow-lg animate-scalePulse text-balance
+                    ${mode === "light" ? "text-[#571111] text-shadow-black/20" : "text-[#60A5FA] text-shadow-white/40"}
+                `}
+        >
+          Thales Romanelli
+        </div>
+      </div>
+      <div
+        className={`font-space duration-3000 ease-linear text-balance
+            text-shadow-lg text-[3rem]
+            ${mode === "light" ? "text-[#A87777] text-shadow-black/40" : "text-[#60A5FA] text-shadow-white/40"} 
+            `}
+      >
+        <TypeAnimation
+          preRenderFirstString={true}
+          sequence={[
+            500,
+            "AI & Data Engineer",
+            2000,
+            "Software Engineer",
+            3500,
+            "Full stack developer",
+            500,
+          ]}
+          speed={50}
+          repeat={Infinity}
+        />
+      </div>
+    </div>
+  );
+}
+export default Background;
