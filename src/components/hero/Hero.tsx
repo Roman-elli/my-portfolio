cd553d72a583f15e802f5075f13c5fd096a293f8
Thales Lopes <thalesbrl@outlook.com>
2025-09-27 15:55:13 +0100

    feat(background-animation): finish working on hero text background design

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,18 +6,20 @@ type BackgroundProps = {
 function Hero({ mode }: BackgroundProps) {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full gap-5 pt-[12%] z-10">
-      <div className="flex flex-row gap-6 font-space items-baseline">
+      <div className="flex flex-row gap-6 font-space items-baseline animate-scalePulse">
         <div
           className={`duration-3000 ease-linear text-shadow-lg text-balance text-[2rem]
                     ${mode === "light" ? "text-black text-shadow-black/20" : "text-white text-shadow-white/40"}
                 `}
+          data-testid="hi-test-id"
         >
           Hi. I'm
         </div>
         <div
-          className={`duration-3000 ease-linear text-shadow-lg animate-scalePulse text-balance text-[2.5rem]
+          className={`duration-3000 ease-linear text-shadow-lg  text-balance text-[2.5rem]
                     ${mode === "light" ? "text-[#571111] text-shadow-black/20" : "text-[#60A5FA] text-shadow-white/40"}
                 `}
+          data-testid="name-test-id"
         >
           Thales Romanelli
         </div>
@@ -27,6 +29,7 @@ function Hero({ mode }: BackgroundProps) {
             text-shadow-lg text-[3rem]
             ${mode === "light" ? "text-[#A87777] text-shadow-black/40" : "text-[#95C1F8] text-shadow-white/60"} 
             `}
+        data-testid="writer-test-id"
       >
         <TypeAnimation
           preRenderFirstString={true}
