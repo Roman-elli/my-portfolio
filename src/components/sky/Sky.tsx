49727996fe45a7397d2d46a083095437bd068b2f
Thales Lopes <thalesbrl@outlook.com>
2025-10-02 22:28:51 +0100

    feat(sky-background-component): added all clouds and its animation effects

--- src/components/sky/Sky.tsx
+++ src/components/sky/Sky.tsx
@@ -1,22 +1,149 @@
 import { useState, useEffect } from "react";
+import "./Sky.css";
 import Sun from "../../assets/images/sky/sun.png";
 import Moon from "../../assets/images/sky/moon.png";
 import cloudA from "../../assets/images/clouds/cloudA.png";
-//import cloudB from "../../assets/images/clouds/cloudB.png"
-//import cloudC from "../../assets/images/clouds/cloudC.png"
-//import cloudD from "../../assets/images/clouds/cloudD.png"
-//import cloudE from "../../assets/images/clouds/cloudE.png"
-//import cloudF from "../../assets/images/clouds/cloudF.png"
-//import cloudG from "../../assets/images/clouds/cloudG.png"
-//import cloudH from "../../assets/images/clouds/cloudH.png"
+import cloudB from "../../assets/images/clouds/cloudB.png";
+import cloudC from "../../assets/images/clouds/cloudC.png";
+import cloudD from "../../assets/images/clouds/cloudD.png";
+import cloudE from "../../assets/images/clouds/cloudE.png";
+import cloudF from "../../assets/images/clouds/cloudF.png";
+import cloudG from "../../assets/images/clouds/cloudG.png";
+import cloudH from "../../assets/images/clouds/cloudH.png";
 
 type SkyProps = {
   mode: "light" | "dark";
 };
 
+type Cloud = {
+  src: string;
+  animate: string;
+  pt: string;
+  pl: string;
+  initialTranslate: string;
+};
+
 function Sky({ mode }: SkyProps) {
   const [sunVisible, setSunVisible] = useState(mode === "light");
   const [moonVisible, setMoonVisible] = useState(mode === "dark");
+
+  const clouds: Cloud[] = [
+    {
+      src: cloudA,
+      animate: "animate-cloudA",
+      pt: "30%",
+      pl: "25%",
+      initialTranslate: "-200%",
+    },
+    {
+      src: cloudB,
+      animate: "animate-cloudB",
+      pt: "18%",
+      pl: "70%",
+      initialTranslate: "200%",
+    },
+    {
+      src: cloudC,
+      animate: "animate-cloudC",
+      pt: "19%",
+      pl: "15%",
+      initialTranslate: "-200%",
+    },
+    {
+      src: cloudD,
+      animate: "animate-cloudD",
+      pt: "30%",
+      pl: "60%",
+      initialTranslate: "200%",
+    },
+    {
+      src: cloudE,
+      animate: "animate-cloudE",
+      pt: "5%",
+      pl: "35%",
+      initialTranslate: "-200%",
+    },
+    {
+      src: cloudF,
+      animate: "animate-cloudF",
+      pt: "5%",
+      pl: "70%",
+      initialTranslate: "200%",
+    },
+    {
+      src: cloudG,
+      animate: "animate-cloudG",
+      pt: "5%",
+      pl: "10%",
+      initialTranslate: "-200%",
+    },
+    {
+      src: cloudH,
+      animate: "animate-cloudH",
+      pt: "18%",
+      pl: "50%",
+      initialTranslate: "200%",
+    },
+  ];
+
+  const back_clouds: Cloud[] = [
+    {
+      src: cloudA,
+      animate: "animate-cloudB",
+      pt: "10%",
+      pl: "30%",
+      initialTranslate: "-200%",
+    },
+    {
+      src: cloudB,
+      animate: "animate-cloudH",
+      pt: "20%",
+      pl: "40%",
+      initialTranslate: "200%",
+    },
+    {
+      src: cloudC,
+      animate: "animate-cloudD",
+      pt: "30%",
+      pl: "70%",
+      initialTranslate: "-200%",
+    },
+    {
+      src: cloudD,
+      animate: "animate-cloudA",
+      pt: "20%",
+      pl: "60%",
+      initialTranslate: "200%",
+    },
+    {
+      src: cloudE,
+      animate: "animate-cloudE",
+      pt: "30%",
+      pl: "40%",
+      initialTranslate: "-200%",
+    },
+    {
+      src: cloudF,
+      animate: "animate-cloudC",
+      pt: "30%",
+      pl: "20%",
+      initialTranslate: "200%",
+    },
+    {
+      src: cloudG,
+      animate: "animate-cloudG",
+      pt: "15%",
+      pl: "50%",
+      initialTranslate: "-200%",
+    },
+    {
+      src: cloudH,
+      animate: "animate-cloudF",
+      pt: "10%",
+      pl: "60%",
+      initialTranslate: "200%",
+    },
+  ];
 
   useEffect(() => {
     if (mode === "light") {
@@ -47,16 +174,32 @@ function Sky({ mode }: SkyProps) {
                 `}
         />
       </div>
-
-      <div className="flex w-full h-full fixed pt-[10%] pl-[40%]">
-        <img
-          src={cloudA}
-          alt="Bright white moon in the sky"
-          className={`w-40 h-30 transition-all duration-2500 ease-in-out fixed
-                    ${sunVisible ? "" : "translate-x-[-400%] opacity-0"}
-                `}
-        />
-      </div>
+      {back_clouds.map((cloud, i) => (
+        <div
+          key={i}
+          className={`flex w-full h-full fixed pt-[${cloud.pt}] pl-[${cloud.pl}] ${cloud.animate}`}
+        >
+          <img
+            src={cloud.src}
+            alt={`Cloud ${i + 1}`}
+            className={`w-[6vw] h-[5vw] transition-all duration-3000 ease-in-out fixed animate-scalePulse drop-shadow-black/40 drop-shadow-md
+                    ${sunVisible ? "opacity-40" : `translate-x-[${cloud.initialTranslate}] opacity-0`}`}
+          />
+        </div>
+      ))}
+      {clouds.map((cloud, i) => (
+        <div
+          key={i}
+          className={`flex w-full h-full fixed pt-[${cloud.pt}] pl-[${cloud.pl}] ${cloud.animate}`}
+        >
+          <img
+            src={cloud.src}
+            alt={`Cloud ${i + 1}`}
+            className={`w-[12vw] h-[8vw] transition-all duration-2000 ease-in-out fixed animate-scalePulse drop-shadow-yellow-400/30 drop-shadow-lg
+                    ${sunVisible ? "opacity-95" : `translate-x-[${cloud.initialTranslate}] opacity-0`}`}
+          />
+        </div>
+      ))}
     </div>
   );
 }
