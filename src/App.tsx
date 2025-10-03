2c67fadf62b63e96503b3946d79cc38dc1a9ae33
Thales Lopes <thalesbrl@outlook.com>
2025-10-04 00:03:03 +0100

    refactor(feat/sky-background-component): reestructure code to allow animation when reload page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import Header from "./components/header/Header.tsx";
 import Hero from "./components/hero/Hero.tsx";
 import Sky from "./components/sky/Sky.tsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [mode, setMode] = useState<"light" | "dark">("light");
   const [isAnimating, setIsAnimating] = useState(false);
+  const [isStart, setIsStart] = useState(true);
+
+  useEffect(() => {
+    setIsStart(false);
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsAnimating(false);
+    }, 2500);
+  }, []);
 
   const toggleMode = () => {
     setMode((prev) => (prev === "light" ? "dark" : "light"));
@@ -19,11 +28,17 @@ function App() {
     <div
       className={`w-screen h-screen transition-colors duration-5000 ease-linear
     ${mode === "light" ? "bg-light" : "bg-dark"} 
+    ${isStart ? "bg-light/30" : ""}
     `}
     >
-      <Header mode={mode} isAnimating={isAnimating} toggleMode={toggleMode} />
-      <Sky mode={mode} />
-      <Hero mode={mode} />
+      <Header
+        mode={mode}
+        isAnimating={isAnimating}
+        toggleMode={toggleMode}
+        isStart={isStart}
+      />
+      <Sky mode={mode} isStart={isStart} />
+      <Hero mode={mode} isStart={isStart} />
     </div>
   );
 }
