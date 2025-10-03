12a858d279749eeca875bdde1b2c26e8388d4c6d
Thales Lopes <thalesbrl@outlook.com>
2025-10-03 10:20:59 +0100

    refactor(feat/sky-background-component): refactor hero responsive method

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,7 +8,7 @@ function Hero({ mode }: BackgroundProps) {
     <div className="flex flex-col items-center justify-center w-full h-full gap-5 pt-[12%] z-8 fixed">
       <div className="flex flex-row gap-6 font-space items-baseline animate-scalePulse">
         <div
-          className={`duration-5000 ease-linear text-shadow-lg text-balance text-[2rem]
+          className={`duration-5000 ease-linear text-shadow-lg text-balance text-[clamp(1.2rem,2vw,2rem)]
                     ${mode === "light" ? "text-black text-shadow-black/20" : "text-white text-shadow-white/40"}
                 `}
           data-testid="hi-test-id"
@@ -16,7 +16,7 @@ function Hero({ mode }: BackgroundProps) {
           Hi. I'm
         </div>
         <div
-          className={`duration-5000 ease-linear text-shadow-lg  text-balance text-[2.5rem]
+          className={`duration-5000 ease-linear text-shadow-lg  text-balance text-[clamp(1.5rem,3vw,2.5rem)]
                     ${mode === "light" ? "text-[#571111] text-shadow-black/20" : "text-[#60A5FA] text-shadow-white/40"}
                 `}
           data-testid="name-test-id"
@@ -26,7 +26,7 @@ function Hero({ mode }: BackgroundProps) {
       </div>
       <div
         className={`font-space duration-5000 ease-linear text-balance
-            text-shadow-lg text-[3rem]
+            text-shadow-lg text-[clamp(1.8rem,4vw,3rem)]
             ${mode === "light" ? "text-[#A87777] text-shadow-black/60" : "text-[#95C1F8] text-shadow-white/60"} 
             `}
         data-testid="writer-test-id"
