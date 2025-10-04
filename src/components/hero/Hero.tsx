ae08328b82f39358ed5a8468c65a5d5437efe2ce
Thales Lopes <thalesbrl@outlook.com>
2025-10-04 14:18:57 +0100

    feat(sky-background-component): added new reload page animation on hero component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,15 +1,17 @@
 import { TypeAnimation } from "react-type-animation";
 type BackgroundProps = {
   mode: "light" | "dark";
+  isStart: boolean;
 };
 
-function Hero({ mode }: BackgroundProps) {
+function Hero({ mode, isStart }: BackgroundProps) {
   return (
     <div className="flex flex-col items-center justify-center w-full h-full gap-5 pt-[30vh] z-80 fixed">
       <div className="flex flex-row gap-6 font-space items-baseline animate-scalePulse">
         <div
           className={`duration-5000 ease-linear text-shadow-lg text-balance text-[clamp(1.2rem,2vw,2rem)]
                     ${mode === "light" ? "text-black text-shadow-black/20" : "text-white text-shadow-white/40"}
+                    ${isStart ? "opacity-0" : ""}
                 `}
           data-testid="hi-test-id"
         >
@@ -18,6 +20,7 @@ function Hero({ mode }: BackgroundProps) {
         <div
           className={`duration-5000 ease-linear text-shadow-lg  text-balance text-[clamp(1.5rem,3vw,2.5rem)]
                     ${mode === "light" ? "text-[#571111] text-shadow-black/20" : "text-[#60A5FA] text-shadow-white/40"}
+                    ${isStart ? "opacity-0" : ""}
                 `}
           data-testid="name-test-id"
         >
@@ -28,6 +31,7 @@ function Hero({ mode }: BackgroundProps) {
         className={`font-space duration-5000 ease-linear text-balance
             text-shadow-lg text-[clamp(1.8rem,4vw,3rem)]
             ${mode === "light" ? "text-[#A87777] text-shadow-black/60" : "text-[#95C1F8] text-shadow-white/60"} 
+            ${isStart ? "opacity-0" : ""}
             `}
         data-testid="writer-test-id"
       >
