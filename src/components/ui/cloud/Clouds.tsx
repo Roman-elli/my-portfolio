2a96050469ff891c60818e2060aa92ee52aa68fb
Thales Lopes <thalesbrl@outlook.com>
2025-10-04 14:21:09 +0100

    feat(sky-background-component): added new reload page animation on cloud background component

--- src/components/ui/cloud/Clouds.tsx
+++ src/components/ui/cloud/Clouds.tsx
@@ -1,4 +1,4 @@
-import type { Cloud as CloudType } from "./../../sky/Sky.tsx"; // importa o tipo Clouds
+import type { Cloud as CloudType } from "./../../sky/Sky.tsx";
 import React from "react";
 
 interface CloudProps {
@@ -14,9 +14,10 @@ const Clouds: React.FC<CloudProps> = ({ cloud, visible, back = false }) => {
         src={cloud.src}
         alt={back ? "Clouds back" : "Clouds"}
         className={`h-auto transition-all ease-in-out animate-scalePulse drop-shadow-yellow-400/30
-          ${back ? "w-[clamp(80px,6vw,140px)] drop-shadow-md duration-2000" : "w-[clamp(110px,12vw,200px)] drop-shadow-lg duration-3000"}
+          ${back ? "w-[clamp(40px,6vw,140px)] drop-shadow-md duration-2000" : "w-[clamp(105px,12vw,200px)] drop-shadow-lg duration-3000"}
           ${visible ? cloud.opacity : `${cloud.initialTranslate} opacity-0`}
         `}
+        data-testid="cloud-id"
       />
     </div>
   );
