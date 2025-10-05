3ff8c5be4bccc11e7f78e6c2343a6c15fb14b9be
Thales Lopes <thalesbrl@outlook.com>
2025-10-05 14:29:51 +0100

    feat(contact-component): added new contact component

---
+++ src/components/contact/Contact.tsx
@@ -0,0 +1,54 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import "./Contact.css";
+type ContactProps = {
+  mode: "light" | "dark";
+  isStart: boolean;
+  isAnimating: boolean;
+};
+
+function Contact({ mode, isStart, isAnimating }: ContactProps) {
+  return (
+    <div
+      className={`fixed bottom-0 left-[8vw] flex flex-col z-90 items-center space-y-4 duration-5000
+        ${isStart ? "translate-y-[50%] opacity-0" : ""}
+        `}
+    >
+      <a
+        href="https://github.com/Roman-elli"
+        target="_blank"
+        rel="noreferrer"
+        className="cursor-pointer"
+      >
+        <FaGithub
+          className={` w-[clamp(2rem,2.3vw,4vw)] h-auto hover:scale-110 ease-in-out drop-shadow-md hover:animate-contactButtonBounce
+                ${mode === "light" ? "text-[#571111] drop-shadow-white/70 hover:text-white hover:drop-shadow-white" : " text-[#95C1F8] drop-shadow-yellow-400/70 hover:text-white hover:drop-shadow-yellow-400"}
+                ${isAnimating ? "duration-2500" : "duration-400"}
+                
+                `}
+        />
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/thales-romanelli/"
+        target="_blank"
+        rel="noreferrer"
+        className="cursor-pointer"
+      >
+        <FaLinkedin
+          className={` w-[clamp(2rem,2.3vw,4vw)] h-auto hover:scale-110 ease-in-out drop-shadow-md hover:animate-contactButtonBounce
+                ${mode === "light" ? "text-[#571111] drop-shadow-white/70 hover:text-white hover:drop-shadow-shadow-white" : " text-[#95C1F8] drop-shadow-yellow-400/50 hover:text-white hover:drop-shadow-shadow-yellow-400"}
+                ${isAnimating ? "duration-2500" : "duration-400"}
+                `}
+        />
+      </a>
+
+      <div
+        className={`w-[4px] rounded-full duration-2500 transition-all ease-in-out h-[clamp(8vh,12vh,18vh)]
+              ${mode === "light" ? "bg-[#571111]/60 shadow-[0_0_10px_#ffffff]" : "bg-[#60A5FA] shadow-[0_0_10px_#FACC15]"}
+            `}
+      ></div>
+    </div>
+  );
+}
+
+export default Contact;
