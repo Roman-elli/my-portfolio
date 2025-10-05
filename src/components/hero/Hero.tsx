3a8613ccc9cf91b6f68449c04ebe80769d078381
Thales Lopes <thalesbrl@outlook.com>
2025-10-05 14:29:04 +0100

    update(feat/contact-component): update some colors on the hero component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -7,10 +7,10 @@ type BackgroundProps = {
 function Hero({ mode, isStart }: BackgroundProps) {
   return (
     <div className="flex flex-col items-center justify-center w-full h-[100vh] gap-5 pt-[30vh] z-80 fixed">
-      <div className="flex flex-row gap-6 font-space items-baseline animate-scalePulse">
+      <div className="flex flex-row gap-3 font-space items-baseline animate-scalePulse">
         <div
-          className={`duration-5000 ease-linear text-shadow-lg text-balance text-[clamp(1.2rem,2vw,2rem)]
-                    ${mode === "light" ? "text-black text-shadow-black/20" : "text-white text-shadow-white/40"}
+          className={`duration-5000 ease-linear text-shadow-lg text-balance text-[clamp(1.5rem,2vw,2rem)]
+                    ${mode === "light" ? "text-rose-950/90 text-shadow-black/20" : "text-white text-shadow-white/40"}
                     ${isStart ? "opacity-0" : ""}
                 `}
           data-testid="hi-test-id"
@@ -18,8 +18,8 @@ function Hero({ mode, isStart }: BackgroundProps) {
           Hi. I'm
         </div>
         <div
-          className={`duration-5000 ease-linear text-shadow-lg  text-balance text-[clamp(1.5rem,3vw,2.5rem)]
-                    ${mode === "light" ? "text-[#571111] text-shadow-black/20" : "text-[#60A5FA] text-shadow-white/40"}
+          className={`duration-5000 ease-linear text-shadow-lg  text-balance text-[clamp(1.8rem,3vw,2.5rem)]
+                    ${mode === "light" ? "text-rose-950/90 text-shadow-black/20" : "text-[#60A5FA] text-shadow-white/40"}
                     ${isStart ? "opacity-0" : ""}
                 `}
           data-testid="name-test-id"
@@ -30,7 +30,7 @@ function Hero({ mode, isStart }: BackgroundProps) {
       <div
         className={`font-space duration-5000 ease-linear text-balance
             text-shadow-lg text-[clamp(1.8rem,4vw,3rem)]
-            ${mode === "light" ? "text-[#A87777] text-shadow-black/60" : "text-[#95C1F8] text-shadow-white/60"} 
+            ${mode === "light" ? "text-white/80 text-shadow-black/60" : "text-[#95C1F8] text-shadow-white/60"} 
             ${isStart ? "opacity-0" : ""}
             `}
         data-testid="writer-test-id"
