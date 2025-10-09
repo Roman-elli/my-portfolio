cec00ac4c5da4bb8f3336b408269736e719ab054
Thales Lopes <thalesbrl@outlook.com>
2025-10-09 22:49:32 +0100

    fix(main): fix chunk size limit

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    build: {
+        chunkSizeWarningLimit: 2000,
+    },
     test: {
         globals: true,
         environment: "jsdom",
